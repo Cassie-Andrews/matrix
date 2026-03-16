67461fd59425dbebbd58d87e78605aecc790108a
Cassie Andrews <candrews1@ufl.edu>
2026-03-15 21:01:06 -0400

    update pages

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Hello world!</h1>
     </main>
-  )
+  );
 
   /*const card = await getCard();
   
