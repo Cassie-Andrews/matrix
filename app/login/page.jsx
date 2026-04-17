a2fd860b426675489d4b70cd58e6e455b1aeb129
Cassie Andrews <candrews1@ufl.edu>
2026-04-17 00:01:48 -0400

    update sign up

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -15,7 +15,6 @@ export default async function LoginPage({ searchParams }) {
                         <p>Username</p>
                         <input
                             label="Username"
-                            className="textInput"
                             name="username"
                             type="text"
                             placeholder="Username" 
@@ -24,7 +23,6 @@ export default async function LoginPage({ searchParams }) {
                         <p>Password</p>
                         <input 
                             label="Password"
-                            className="textInput"
                             name="password" 
                             type="password" 
                             placeholder="Password" 
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -10,35 +10,41 @@ export default function SignupPage() {
 
     return (
         <>
-            <div className={styles.signupContainer}> 
-                <h3>Create An Account</h3>
-                {error && <p>{error}</p>}
-                <form 
-                    action={formAction} 
-                    className={styles.signupForm}
-                >
-                    <input 
-                        className="textInput"
-                        name="username" 
-                        type="text" 
-                        placeholder="Username" 
-                        required
-                    />
-                    <input 
-                        className="textInput"
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        required
-                    />
-                    <button 
-                        type="submit" 
-                        className="primaryButton"
+            <div className={styles.contentContainer}>
+                <div className={styles.signupContainer}> 
+                    <h3>Create An Account</h3>
+                    {error && <p>{error}</p>}
+                    <form 
+                        action={formAction} 
+                        className={styles.signupForm}
                     >
-                        Sign Up
-                    </button>
-                </form>
-                <p>Already have an account? <a href="/login">Login</a></p>
+                        <p>Username</p>
+                        <input 
+                            label="username"
+                            className="textInput"
+                            name="username" 
+                            type="text" 
+                            placeholder="Username" 
+                            required
+                        />
+                        <p>Password</p>
+                        <input 
+                            label="password"
+                            className="textInput"
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            required
+                        />
+                        <button 
+                            type="submit" 
+                            className={styles.primaryButton}
+                        >
+                            Sign Up
+                        </button>
+                    </form>
+                    <p>Already have an account? <a href="/login">Login</a></p>
+                </div>
             </div>
         </>
     )
