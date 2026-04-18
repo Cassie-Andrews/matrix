1e09bf6c8fd4089aff4ff57bea4b87de76a866f9
Cassie Andrews <candrews1@ufl.edu>
2026-04-18 18:02:47 -0400

    update nav

--- app/components/bottomNav/BottomNav.jsx
+++ app/components/bottomNav/BottomNav.jsx
@@ -2,6 +2,7 @@ import styles from './BottomNav.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useTimer } from '@/app/context/TimerContext';
+import { logout } from "../../actions/auth";
 
 
 export default function BottomNav() {
@@ -9,39 +10,21 @@ export default function BottomNav() {
 
     return (
         <nav className={styles.bottomNav}>
+            <div className={styles.navContent}>
             <ul className={styles.navLinks}>
-
-            {/* Dashboard/Home */}
+            {/* Punch Cards */}
                 <li className={styles.navLink}>
                     <Link href="/" className={styles.navLinkContent}>
                         <Image 
-                            src="/ICON_default_home.png"
-                            alt="default home icon"
-                            width={40}
-                            height={40}
-                        />
-                        <Image 
-                            src="/ICON_active_home.png"
-                            alt="Home icon active"
-                            width={40}
-                            height={40}
-                            className={styles.hoverIcon}
-                        />
-                    </Link>
-                </li>
-            {/* Punch Cards */}
-                <li className={styles.navLink}>
-                    <Link href="#" className={styles.navLinkContent}>
-                        <Image 
                             src="/ICON_default_check_box.png"
-                            alt="Checkbox icon default"
+                            alt="dashboard"
                             width={40}
                             height={40}
                             className={styles.defaultIcon}
                         />
                         <Image 
                             src="/ICON_active_check_box.png"
-                            alt="Check box icon active"
+                            alt="dashboard"
                             width={40}
                             height={40}
                             className={styles.hoverIcon}
@@ -56,14 +39,14 @@ export default function BottomNav() {
                     >
                         <Image 
                             src="/ICON_default_timer.png"
-                            alt="default timer icon"
+                            alt="timer"
                             width={40}
                             height={40}
                             className={styles.defaultIcon}
                         />
                         <Image 
                             src="/ICON_active_timer.png"
-                            alt="Timer icon active"
+                            alt="timer"
                             width={40}
                             height={40}
                             className={styles.hoverIcon}
@@ -73,24 +56,28 @@ export default function BottomNav() {
 
             {/* Settings */}
                 <li className={styles.navLink}>
-                    <Link href="/settings" className={styles.navLinkContent}>
+                    <button
+                        className={styles.navLinkContent}
+                        onClick={ logout }
+                    >
                         <Image 
-                            src="/ICON_default_settings.png"
-                            alt="default settings icon"
+                            src="/ICON_default_logout.png"
+                            alt="Log out"
                             width={40}
                             height={40}
                             className={styles.defaultIcon}
                         />
                         <Image 
-                            src="/ICON_active_settings.png"
-                            alt="Settings icon active"
+                            src="/ICON_active_logout.png"
+                            alt="Log out"
                             width={40}
                             height={40}
                             className={styles.hoverIcon}
                         />
-                    </Link>
+                    </button>
                 </li>
             </ul>
+            </div>
         </nav>
     )
 }
