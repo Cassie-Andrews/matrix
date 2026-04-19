e71e99384551ad3c1a3205a2d8e3b296b2f5cdfe
Cassie Andrews <candrews1@ufl.edu>
2026-04-19 16:07:58 -0400

    add/edit card modal - responsive

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
+            {/*
             <ul className={styles.footerLinks}>
                 <li className={styles.footerLink}><Link href="#">Privacy Policy</Link></li>|
                 <li className={styles.footerLink}><Link href="#">Terms and Conditions</Link></li>
-            </ul>
-            <p className={styles.footerText}> &copy; 2026 Matrix. All rights reserved.</p>
+            </ul> 
+            */}
+            <p className={styles.footerText}> &copy; 2026 Cassie Andrews. All rights reserved.</p>
         </footer>
     )
 }
