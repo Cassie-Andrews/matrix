fbf7ac90d760c808aa793a687cc4abf0d70a5528
Cassie Andrews <candrews1@ufl.edu>
2026-04-19 21:16:51 -0400

    timer responsive resize

--- app/context/TimerContext.js
+++ app/context/TimerContext.js
@@ -1,22 +1,55 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const TimerContext = createContext();
 
 const toMinutes = (seconds) => Math.round(seconds / 60);
 const toSeconds = (minutes) => minutes * 60;
 
+const getInitialSettings = () => {
+    try {
+        if (typeof winow === "undefined") return null;
+    
+        const saved = localStorage.getItem("timerSettings");
+        if (!saved) return;
+    
+        return JSON.parse(saved);
+    } catch (err) {
+        console.error('Failed to load timer settings:', err)
+        return null;
+    }
+}
+
 export const TimerProvider = ({ children }) => {
     const [ showTimer, setShowTimer ] = useState(false)
+    
+    const savedSettings = getInitialSettings();
 
-    const [durations, setDurations] = useState({
-        focus: 25 * 60,
-        "short break": 5 * 60,
-        "long break": 15 * 60,
-    });
+    const [durations, setDurations] = useState(
+        savedSettings?.durations || {
+            focus: 25 * 60,
+            "short break": 5 * 60,
+            "long break": 15 * 60,
+        }
+    );
 
-    const [autoCycle, setAutoCycle] = useState(false);
+    const [autoCycle, setAutoCycle] = useState(
+        typeof savedSettings?.autoCycle === "boolean"
+        ? savedSettings.autoCycle
+        : false
+    );
+
+    useEffect(() => {
+        try {
+            localStorage.setItem(
+                "timerSettings",
+                JSON.stringify({ durations, autoCycle })
+            );
+        } catch (err) {
+            console.error("Error saving timer settings", err);
+        } 
+    }, [durations, autoCycle]);
 
     return (
         <TimerContext.Provider 
@@ -29,8 +62,9 @@ export const TimerProvider = ({ children }) => {
                 setAutoCycle,
                 toMinutes,
                 toSeconds
-            }}>
-                {children}
+            }}
+        >
+            {children}
         </TimerContext.Provider>
     );
 }
